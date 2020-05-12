8d0bc22bfd9204498760d04fd8c643a52a822f70
Muh. Fitratul Akbar <64568595+MuhFitratulAkbar@users.noreply.github.com>
2020-05-12 20:21:42 +0800

    Add files via upload
    Buat program menampilkan NIM masing – masing mahasiswa.

---
+++ jobsheet 1 C++.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"Nama : Muh. Fitratul Akbar\n";
+	cout<<"Nim : F1B019093\n";
+	cout<<"Kelompok: Kelompok 19\n\n";
+int nim[13]= {1,11,12,13,4,6,100,20,30,40,50,15,16};
+	
+	cout << "Nim ke-1 adalah " << nim[0] << endl;
+	cout << "Nim ke-2 adalah " << nim[1] << endl;
+	cout << "Nim ke-3 adalah " << nim[2] << endl;
+	cout << "Nim ke-4 adalah " << nim[3] << endl;
+	cout << "Nim ke-5 adalah " << nim[4] << endl;
+    cout << "Nim ke-6 adalah " << nim[5] << endl;
+    cout << "Nim ke-7 adalah " << nim[6] << endl;
+    cout << "Nim ke-8 adalah " << nim[7] << endl;
+    cout << "Nim ke-9 adalah " << nim[8] << endl;
+    cout << "Nim ke-10 adalah " << nim[9] << endl;
+    cout << "Nim ke-11 adalah " << nim[10] << endl;
+    cout << "Nim ke-12 adalah " << nim[11] << endl;
+    cout << "Nim ke-13 adalah " << nim[12] << endl;
+}
+
