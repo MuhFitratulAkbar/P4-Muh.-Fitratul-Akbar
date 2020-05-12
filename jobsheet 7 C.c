a49a3e121cb9c84b743819270d5de246f60735f5
Muh. Fitratul Akbar <64568595+MuhFitratulAkbar@users.noreply.github.com>
2020-05-12 20:29:56 +0800

    Add files via upload
    Buatlah program mengisi nilai array dengan perulangan dengan index array dan bata spengulangan menggunakan NIM
    Terakhir setiap mahasiswa

---
+++ jobsheet 7 C.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+main (){
+printf ("Nama : Muh. Fitratul Akbar\n");
+printf ("Nim : F1B019093\n ");
+printf ("Kelompok : 19\n\n");
+
+int nim [13], i;
+for (i = 0; i<= 13; i++){
+printf ("Masukkan Nim ke-%i :", i+1);
+scanf ("%i", &nim[i]);
+}
+}
+
