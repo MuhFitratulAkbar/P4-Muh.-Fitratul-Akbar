18d07fcadb4f63e612bbc5325c6a03dcabb51910
Muh. Fitratul Akbar <64568595+MuhFitratulAkbar@users.noreply.github.com>
2020-05-12 20:30:15 +0800

    Add files via upload
    Buatlah program mengisi nilai array dengan perulangan dengan index array dan bata spengulangan menggunakan NIM
    Terakhir setiap mahasiswa

---
+++ jobsheet 7 C++.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+main (){
+cout<<"Nama : Muh. Fitratul Akbar\n";
+cout<<"Nim : F1B019093\n";
+cout<<"Kelompok: Kelompok 19\n\n";
+int nim [13];
+for (int i = 0; i<= 13; i++){
+cout<< "Masukkan nim ke-1 : " << i+1;
+cin>>nim[i];
+}
+}
+
