f4129cc799ece5468295b0e463380989672a5fd3
Muh. Fitratul Akbar <64568595+MuhFitratulAkbar@users.noreply.github.com>
2020-05-12 20:28:48 +0800

    Add files via upload
    Buatlah matriks transpose bersifat dinamis menggunakan persoalan di atas.

---
+++ jobsheet 6 C++.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+main (){
+cout<<"Nama : Muh. Fitratul Akbar\n";
+cout<<"Nim : F1B019093\n";
+cout<<"Kelompok: Kelompok 19\n\n";
+int x,y, mtrx [5][5];
+cout<< "TRANSPOSE MATRIX" <<endl;
+cout<< "Masukkan jumlah baris = ";
+cin>> x;
+cout<< "Masukkan jumlah kolom = ";
+cin>> y;
+cout<<"MATRIX\n";
+for (int i = 0; i< x; i++){
+for (int j = 0; j < y; j++){
+cout<< "Masukkan nilai index " <<i<< "," << j << " = ";
+cin>>mtrx [i][j];
+}
+}
+cout<<endl;
+for (int i = 0; i< x; i++){
+for (int j = 0; j < y; j++){
+cout<<mtrx [i][j] <<"  ";
+}
+cout<<endl;
+}
+cout<<"TRANSPOSE MATRIX\n";
+for (int i = 0; i< y; i++){
+for (int j = 0; j < x; j++){
+cout<<mtrx [j][i] <<"  ";
+}
+cout<<endl;
+}
+}
+
