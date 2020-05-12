d998a637ac46fd3391f910afbae142508ea6f627
Muh. Fitratul Akbar <64568595+MuhFitratulAkbar@users.noreply.github.com>
2020-05-12 20:37:37 +0800

    Add files via upload
    Buatlah program matriks tiga dimensi dengan menggunakan nim tiga orang mahasiswa

---
+++ jobsheet 10 C++.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+main (){
+cout<<"Nama : Muh. Fitratul Akbar\n";
+cout<<"Nim : F1B019093\n";
+cout<<"Kelompok: Kelompok 19\n\n";
+
+char nama [2][2][10];
+int i, j;
+for (i = 0; i< 2; i++){
+for (j = 1; j < 10; j++){
+cout<< "nama my best friend  [" <<i<<"][" << j << "] = ";
+cin>>nama[i][j];
+}
+}
+}
+
