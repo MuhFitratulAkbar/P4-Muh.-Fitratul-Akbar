1ee0f77c3ddb9217fb9b064ce0088eb9ee433dc0
Muh. Fitratul Akbar <64568595+MuhFitratulAkbar@users.noreply.github.com>
2020-05-13 08:29:25 +0800

    Update Studi Kasus.cpp
    1.	Buatlah program perhitungan belanja yang meliputi nama barang, harga barang, jumlah barang, total dengan ketentuan
    a.	Menggunakan modul 1 sampai 4
    b.	Total belanja lebih dari 200000 diskon sesuai dua nim terakhir
    c.	Dikerjakan oleh KELOMPOK 19
           dan menambahkan menu jasa

--- Studi Kasus.cpp
+++ Studi Kasus.cpp
@@ -3,7 +3,7 @@
 #include <iomanip>
 #include <math.h>
 using namespace std;
-int input(){
+void pembelian(){
     int jum_beli, bayar, diskon, jumlah[50], harga[50], sub_tot[50];
 	string nama[100];
     float tot;
@@ -25,7 +25,7 @@ int input(){
 	}
 	cout<<endl;
 	cout<<"Struk Belanja"<<endl;   
-               cout<<"|============================================|"<<endl;
+    cout<<"|============================================|"<<endl;
 	cout<<"No   Barang    Jumlah     Harga     Sub Total"<<endl;
 	for (int i=0;i<jum_beli;i++)
 	{    cout<<i+1<<setw(8)<<nama[i]<<setw(10)<<jumlah[i]<<setw(12)<<harga[i]<<setw(12)<<sub_tot[i]<<endl; 
@@ -36,22 +36,22 @@ int input(){
 	    cout <<"mendapat diskon 93 %"<<endl;
 	    diskon=0.93*tot;
     }
-	    cout<<"Jumlah Bayar : Rp."<<tot<<endl;
-	    cout<<"Dikon        : Rp."<<diskon<<endl; 
-	    cout<<"Total Bayar  : Rp."<<tot-diskon<<endl;
-	    cout<<"Bayar        : Rp.";
+	    cout<<"Total Pembayaran : Rp."<<tot<<endl;
+	    cout<<"Dikon            : Rp."<<diskon<<endl; 
+	    cout<<"Total Bayar      : Rp."<<tot-diskon<<endl;
+	    cout<<"Jumlah Uang      : Rp.";
 	    cin>>bayar; 
-	    cout<<"Kembalian    : Rp."<<(bayar-(tot-diskon))<<endl;  
+	    cout<<"Kembalian        : Rp."<<(bayar-(tot-diskon))<<endl;  
 	    cout<<"==============================================\n";
 }
-int jasa()
+void jasa()
 {
-	int n, bayar, diskon,kembali, berat[50], jarak[50], sub_tot[50],kg=5000;
+	int total, bayar, diskon,kembali, berat[50], jarak[50], sub_tot[50],kg=5000;
 	string nama[100];
     float tot;
     cout<<"Masukan Jumlah Barang yang ingin Di kirim : ";
-	cin>>n; 
-	for (int i=0; i<n;i++)
+	cin>>total; 
+	for (int i=0; i<total;i++)
 	{
 	    cout<<endl;
 	    cout<<"Masukan Barang Ke-"<<i+1<<endl;
@@ -67,50 +67,70 @@ int jasa()
 	    tot+=sub_tot[i]; 
 	    cout<<endl;
     }
-    		cout<<"Total Pembayaran Keseluruhan : Rp. "<<tot<<endl;
-    		cout<<"Bayar     : Rp. ";
-    		cin>>bayar;
-   			cout<<"Kembalian : Rp. "<<bayar-tot<<endl;	
-			cout<<"======================================"<<endl;
-			cout<<"Terima Kasih Telah Menggunakan Jasa Di Toko Kami"<<endl;
+    cout<<endl;
+	cout<<"Struk Pengiriman"<<endl;   
+    cout<<"|============================================|"<<endl;
+	cout<<"No   Barang     Berat       Jarak     Total"<<endl;
+    for (int i=0;i<total;i++)
+	{ cout<<i+1<<setw(8)<<nama[i]<<setw(10)<<berat[i]<<setw(12)<<jarak[i]<<setw(12)<<sub_tot[i]<<endl; 
+	}
+	cout<<"|============================================|"<<endl;
+	if (tot>=200000)
+	{
+	    cout <<"mendapat diskon 93 %"<<endl;
+	    diskon=0.93*tot;
+    }
+    cout<<"Total Pembayaran : Rp."<<tot<<endl;
+	cout<<"Dikon            : Rp."<<diskon<<endl; 
+	cout<<"Total Bayar      : Rp."<<tot-diskon<<endl;
+	cout<<"Jumlah Uang      : Rp.";
+	cin>>bayar; 
+	cout<<"Kembalian        : Rp."<<(bayar-(tot-diskon))<<endl;  
+	cout<<"================================================="<<endl;
+	cout<<"Terima Kasih Telah Menggunakan Jasa Di Toko Kami"<<endl;
 					
-}
-int keluar()
-{
-    keluar:
-    cout<<"==========Terima Kasih Telah Berbelanja Di Toko Kami=========="<<endl;
-    cout<<"=======Silahkan Datang Kembali Jika Anda Berkenan========"<<endl;
 }
 int main()
 {
-	string user,pass;
-	cout<<"==============================="<<endl;
-     cout<<" \tToko MURAH MERIAH"<<endl;
-     cout<<" \tMuh. Fitratul Akbar"<<endl;
-     cout<<"==============================="<<endl;
-     cout<<endl;
-     cout<<"===========MENU LOGIN=========="<<endl<<endl;
-     cout<<"Masukkan UserName : "; 
-	cin>>user;
-     cout<<"Masukkan Password : "; 
-	cin>>pass;
+	int i,keluar;
+    string pass_input, user_input;  
+    string username ="fitratul_akbar";    
+    string password = "F1B019093";    
+    string login = "block";    
+    i = 1;
+    cout<<"==============================="<<endl;
+    cout<<" \tToko MURAH MERIAH"<<endl;
+    cout<<" \tMuh. Fitratul Akbar"<<endl;
+    cout<<"==============================="<<endl;
+    cout<<endl;    
+   
+   do {        
+       cout << "Massukan Username = "; cin>>user_input;        
+       cout << "Massukan Password = "; cin>>pass_input;        
+       if (user_input == username && pass_input == password) {    
+           cout<<endl;
+		   cout << "| Login Berhasil |" << endl;            
+           i = 4;
+           login = "berhasil";        } 
+       else {            
+           cout << "\n===========================\n";            
+           cout << "Username / Password Salah! ("<<i<<"x)\n";
+		   cout << "=============================\n\n";            
+           i = i +1;
+       }
+       }
+ while (i <= 3);
+	
+    if(login != "berhasil"){
+       cout<<"Anda telah 3x salah login.\n";       
+       cout<<"Silahkan coba lagi besok. Terima Kasih..\n\n";      
+       goto awal;
+	}   
  
-     if(user == "fitratul_akbar" && pass == "F1B019093")
-	{
-        cout<<"\n\nPassword dan Username Anda Cocok, Anda Berhasil  Masuk"<<endl;
-        system("pause");
-        system("cls");
-        goto awal;
-     }
-     else{
-        cout<<"Gagal Masuk, Password Atau Username Anda Salah";
-        system("pause");
-        exit(1);
-     }
-    awal :
+ awal :
     int pilihan, pilih; 
     cout<< "====================================="<<endl;
-    cout<< "|    1.Input Barang dan pembayaran  |"<<endl;
+    cout<< "|    1.Pembelian                    |"<<endl;
     cout<< "|    2.jasa pengiriman              |"<<endl;
     cout<< "|    3.Keluar                       |"<<endl;
     cout<< "====================================="<<endl;
@@ -120,7 +140,7 @@ int main()
     {
         case 1:
             system("cls");
-            input();
+            pembelian();
             goto awal;
             break;
         case 2:
@@ -130,9 +150,8 @@ int main()
             break;
         case 3:
         	system("cls");
-  	        keluar:
-  	        keluar();
+         	cout<<"==========Terima Kasih Telah Berbelanja Di Toko Kami=========="<<endl;
+            cout<<"=======Silahkan Datang Kembali Jika Anda Berkenan========"<<endl;
     }
     return 0;
 }
-
